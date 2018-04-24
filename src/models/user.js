4e7870b57159dc1840bb5e68ff5afb7d87c9e829
makkhawan voraboot <pah-bravo@windowslive.com>
2018-04-24 07:17:34 +0700

    MongoDB: Added schema registered date

--- src/models/user.js
+++ src/models/user.js
@@ -2,5 +2,6 @@ const mongoose = require('mongoose');
 
 var Wallet = new mongoose.Schema({
   domain: mongoose.Schema.Types.String,
-  callback_path: mongoose.Schema.Types.String,
+  registed: mongoose.Schema.Types.Date,
+  callback_path: mongoose.Schema.Types.String
 });
