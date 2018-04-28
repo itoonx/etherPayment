aadf43ba1ecabaff696dd42da9e641c630ae9cd2
itoonx <pah-bravo@windowslive.com>
2018-04-28 09:30:00 +0700

    Express: Added express configuration and winston logger middleware

---
+++ src/config/express.js
@@ -0,0 +1,45 @@
+
+const express = require('express');
+const app = express();
+
+const expressWinston = require('express-winston');
+const winstonInstance = require('./winston');
+const httpStatus = require('http-status');
+const APIError = require('../helpers/APIError');
+
+// enable detailed API logging in dev env
+if (process.env.ENV === 'development') {
+  expressWinston.requestWhitelist.push('body');
+  expressWinston.responseWhitelist.push('body');
+  app.use(expressWinston.logger({
+    winstonInstance,
+    meta: true, // optional: log meta data about request (defaults to true)
+    msg: 'HTTP {{req.method}} {{req.url}} {{res.statusCode}} {{res.responseTime}}ms',
+    colorStatus: true // Color the status code (default green, 3XX cyan, 4XX yellow, 5XX red).
+  }));
+}
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+// catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new APIError('API not found', httpStatus.NOT_FOUND);
+  return next(err);
+});
+
+// log error in winston transports except when executing test suite
+if (process.env.ENV !== 'test') {
+  app.use(expressWinston.errorLogger({
+    winstonInstance
+  }));
+}
+
+// error handler, send stacktrace only during development
+app.use((err, req, res, next) => // eslint-disable-line no-unused-vars
+  res.status(err.status).json({
+    message: err.isPublic ? err.message : httpStatus[err.status],
+    stack: process.env.env === 'development' ? err.stack : {}
+  })
+);
+
+module.exports = app;
---
+++ src/config/winston.js
@@ -0,0 +1,13 @@
+
+const winston = require('winston');
+
+const logger = new (winston.Logger)({
+  transports: [
+    new (winston.transports.Console)({
+      json: true,
+      colorize: true
+    })
+  ]
+});
+
+module.exports = logger;
