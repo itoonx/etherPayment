a540bfdefbd06bd3e8295d5d6ca3c427c2399de6
makkhawan voraboot <pah-bravo@windowslive.com>
2018-04-28 07:52:26 +0700

    Added redis and decoded transactions

--- src/ethereum/watcher.js
+++ src/ethereum/watcher.js
@@ -1,25 +1,70 @@
 
+const { promisify } = require('util');
+
 const Web3 = require('web3');
 const chalk = require('chalk');
 
-const Start = () => {
+const redis = require("redis");
+const client = redis.createClient();
 
-  const ProviderEngine = require('web3-provider-engine/index.js');
-  const ZeroClientProvider = require('web3-provider-engine/zero.js');
+const ProviderEngine = require('web3-provider-engine/index.js');
+const ZeroClientProvider = require('web3-provider-engine/zero.js');
 
-  const engine = ZeroClientProvider({
-    getAccounts: function(){},
-    rpcUrl: 'https://mainnet.infura.io/437dYfr0YRK9UHuIyByD',
+const engine = ZeroClientProvider({
+  getAccounts: function(){},
+  rpcUrl: 'https://ropsten.infura.io/437dYfr0YRK9UHuIyByD',
+});
+const web3 = new Web3(engine);
+
+client.on("error", function (err) {
+  throw new Error(err);
+});
+
+const getBlock = (blockhash) => {
+  return new Promise((resolve, reject) => {
+    web3.eth.getBlock(blockhash, (err, res) => {
+      if (err) return reject(err);
+      return resolve(res);
+    });
   });
-  const web3 = new Web3(engine);
+}
+
+const getTransactionByBlockHash = (block) => {
+  return new Promise((resolve) => {
+    return resolve(block.transactions);
+  });
+}
+
+const decodeReceipt = (txid) => {
+  return new Promise((resolve, reject) => {
+    web3.eth.getTransactionReceipt(txid, (err, res) => {
+      if (err) return reject(err);
+      return resolve(res);
+    });
+  });
+}
+
+const Start = () => { 
 
   // log new blocks
-  engine.on('block', (block) => {
-    console.log('================================');
-    console.log('BLOCK CHANGED:', '#'+block.number.toString('hex'), '0x'+block.hash.toString('hex'));
-    console.log('================================');
-  })
+  engine.on('block', async (block) => {
 
+    var blockhash = '0x'+block.hash.toString('hex');
+
+    console.log('================================');
+    console.log('BLOCK CHANGED:', '#'+block.number.toString('hex'), blockhash);
+    console.log('================================');
+    
+    var block = await getBlock(blockhash);
+    var txs = await getTransactionByBlockHash(block);
+
+    txs.map( async (tx, index) => {
+
+      var receipt = await decodeReceipt(tx);
+      
+      
+    });
+  })
 }
 
 module.exports = {
